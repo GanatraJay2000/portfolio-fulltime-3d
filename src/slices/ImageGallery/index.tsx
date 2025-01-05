62959b5ac801962b4643ca6fd4980bf433260aff
Jay Ganatra <ganatrajay2000@gmail.com>
2025-01-05 00:25:00 -0500

    image-gallery for screenshots of projects

--- src/slices/ImageGallery/index.tsx
+++ src/slices/ImageGallery/index.tsx
@@ -1,6 +1,19 @@
+"use client";
+
 import { Content } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import { SliceComponentProps } from "@prismicio/react";
+import { useState } from "react";
+import { Photo, RowsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/rows.css";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 /**
  * Props for `ImageGallery`.
@@ -11,16 +24,30 @@ export type ImageGalleryProps = SliceComponentProps<Content.ImageGallerySlice>;
  * Component for "ImageGallery" Slices.
  */
 const ImageGallery = ({ slice }: ImageGalleryProps): JSX.Element => {
+  const [index, setIndex] = useState(-1);
+  const photos: Photo[] = slice.items.map((item) => ({
+    src: item.image.url!!,
+    alt: item.image.alt ?? ("Image" as any),
+    width: item.image.dimensions?.width ?? 1080,
+    height: item.image.dimensions?.height ?? 810,
+  }));
+
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-5">
-      {slice.items.map((item, index) => (
-        <PrismicNextImage
-          key={index}
-          field={item.image}
-          alt={item.image.alt ?? ("Image" as any)}
-          className="rounded-lg block m-0 p-0"
-        />
-      ))}
+    <div className="">
+      <RowsPhotoAlbum
+        photos={photos}
+        targetRowHeight={120}
+        onClick={({ index }) => setIndex(index)}
+      />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
     </div>
   );
 };
